8830a581b6324d1299201639ebf557b96a3a7018
pjh456 <147148383@qq.com>
2025-12-10 20:36:04 +0800

    refactor: 移除了无用宏

--- include/utils.h
+++ include/utils.h
@@ -3,12 +3,8 @@
 #include "tokenizer.h"
 #include <ctype.h>
 
-#define ONCE(block) \
-    do              \
-    {               \
-        block       \
-    } while (0)
-
+// === 字符判断函数的封装 ===
+// 这些函数封装了 ctype.h，防止传入 signed char 导致的未定义行为，并统一接口
 int is_alpha(unsigned char ch);
 int is_digit(unsigned char ch);
 int is_alnum(unsigned char ch);
