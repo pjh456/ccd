6b9d5206f7aea0fa4f1dafade641bb6e96c26804
pjh456 <147148383@qq.com>
2025-12-12 23:32:07 +0800

    feat: 实现了单元扫描器复合语句与空语句处理

--- src/unit_scanner/unit_scanner_compound.c
+++ src/unit_scanner/unit_scanner_compound.c
@@ -1,11 +1,52 @@
 #include "unit_scanner.h"
 #include "unit_scanner_impl/unit_scanner_impl.h"
 #include "unit_scanner_impl/statement_unit.h"
+#include "tokenizer_impl/token.h"
+#include "vector.h"
 
 StatementUnit *scan_empty(UnitScanner *us)
 {
+    if (!us)
+        return NULL;
+    if (peek_token(us)->type != T_SEMICOLON)
+        return NULL;
+
+    StatementUnit *unit = make_empty_statement_unit(
+        vector_slice(us->tokens, us->pos, us->pos + 1));
+
+    next_token(us); // ;
+    return unit;
 }
 
 StatementUnit *scan_compound(UnitScanner *us)
 {
+    if (!us)
+        return NULL;
+    if (peek_token(us)->type != T_LEFT_BRACE)
+        return NULL;
+
+    size_t pos = us->pos;
+    next_token(us); // {
+
+    Vector *units = vector_new(sizeof(StatementUnit *));
+    while (peek_token(us)->type != T_EOF)
+    {
+        vector_push_back(units, scan_unit(us));
+
+        if (peek_token(us)->type != T_SEMICOLON) // error
+            break;
+        next_token(us); // ;
+
+        if (peek_token(us)->type == T_RIGHT_BRACE)
+        {
+            next_token(us); // }
+            break;
+        }
+    }
+
+    StatementUnit *unit = make_compound_statement_unit(
+        vector_slice(us->tokens, pos, us->pos),
+        units);
+
+    return unit;
 }
